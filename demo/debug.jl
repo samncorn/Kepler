f3c64ac2ef56b50af445b0e850117bf1e1b8c9e6
samncorn <samuel41@illinois.edu>
2025-09-25 11:26:38 -0500

    bugfixing

--- demo/debug.jl
+++ demo/debug.jl
@@ -11,8 +11,14 @@ debug_logger = ConsoleLogger(stderr, Logging.Debug)
 global_logger(debug_logger)
 
 # case that fails in the wild
+# pos = [1.25, 0.0, 0.0]
+# vel = [-0.021759125285088412, -0.015386025041773686, -0.0]
+# dt  = 8.0
+# gm  = 0.0002959122082326087
+
+# parabolic orbit that failed with elliptic upper bound
 pos = [1.25, 0.0, 0.0]
-vel = [-0.021759125285088412, -0.015386025041773686, -0.0]
+vel = [-0.010879562642544206, -0.018843955261014882, -0.0]
 dt  = 8.0
 gm  = 0.0002959122082326087
 
