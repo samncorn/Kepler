059641be83ae61fa321af340eaf6ded85f5e7b9d
samncorn <samuel41@illinois.edu>
2025-10-14 16:19:03 -0500

    1.12 migration

--- demo/debug.jl
+++ demo/debug.jl
@@ -22,14 +22,29 @@ global_logger(debug_logger)
 # dt  = 8.0
 # gm  = 0.0002959122082326087
 
-# another parabolic failure
-pos = [2.5, 0.0, 0.0]
-vel = [0.007693012520886843, 1e-11, -0.0]
-dt  = 8.0
+# # near rectilinear, consider as invalid input
+# pos = [2.5, 0.0, 0.0]
+# vel = [0.007693012520886843, 1e-11, -0.0]
+# dt  = 8.0
+# gm  = 0.0002959122082326087
+
+# failed in the wild
+# runs fine, probably forgot to update to latest version 
+pos = [0.4186942098016838, 0.24637616481752644, 1.3672151662793137]
+vel = [0.023317918469691952, -0.014641183526584653, 0.0008003774012850409]
+dt  = -5.998977942803302
 gm  = 0.0002959122082326087
 
+# test and compare to spice
 posf, velf = Kepler.propagate(pos, vel, dt, gm)
+statef = SPICE.prop2b(gm, [pos..., vel...], dt)
+posf2 = statef[1:3]
+velf2 = statef[4:6]
 
+posf - posf2
+velf - velf2
+
+# break down internals
 DU   = norm(pos)
 TU   = sqrt(DU^3 / gm)
 pos0 = pos/DU
@@ -47,3 +62,6 @@ Evec = cross(vel0, Hvec) - pos0
 e    = norm(Evec)
 
 x = dt0*a/(1 - e)
+
+
+
